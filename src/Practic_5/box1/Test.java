389bcf8e56ee70bba90fb1d94bb23a32c1453c97
Nipup1 <nickolapavlovich@gmail.com>
2023-09-10 00:56:43 +0300

    <

--- src/Practic_5/box1/Test.java
+++ src/Practic_5/box1/Test.java
@@ -1,6 +1,7 @@
 package Practic_5.box1;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.DefaultMenuLayout;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,38 +11,51 @@ public class Test extends GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(900, 450, 300, 300);
 
-        button1.addActionListener(new Event());
-        button2.addActionListener(new Event());
+        button1.addActionListener(new Event1());
+        button2.addActionListener(new Event1());
+        button3.addActionListener(new Event1());
+        button3.setBackground(Color.red);
 
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(button1);
-        bg.add(button2);
 
-        panel.setBackground(Color.GREEN);
-        //panel.setPreferredSize(new Dimension(300,300));
-        panel.setLayout(new GridLayout(3,0));
-        panel.add(button1);
-        panel.add(button2);
+
+
+        panel.setPreferredSize(new Dimension(300,300));
+        grid.add(button1);
+        grid.add(button2);
+        panel.add(grid);
         panel.add(labelScore);
         panel.add(labelGoal);
+        panel.add(labelRez);
+        panel.add(button3);
 
         frame.getContentPane().add(panel);
-        //frame.pack();
+        frame.pack();
         frame.setVisible(true);
     }
 
-    public static class Event extends Test implements ActionListener {
+    public static class Event1 extends Test implements ActionListener {
         public void actionPerformed (ActionEvent e){
 
             if (e.getSource() == button1){
                 x1 += 1;
                 labelScore.setText("Счёт " + x1 + ":" + y1);
+                labelGoal.setText("Последний забивший: Милан");
             }
-            else{
+            else if (e.getSource() == button2){
                 y1 += 1;
                 labelScore.setText("Счёт " + x1 + ":" + y1);
+                labelGoal.setText("Последний забивший: Мадрид");
             }
-            System.out.println(x1 + "\t" + y1);
+            else{
+                if(x1 > y1)
+                    labelRez.setText("Победитель: Милан");
+
+                else if(x1 < y1)
+                    labelRez.setText("Победитель: Мадрид");
+
+                else
+                    labelRez.setText("Победитель: Ничья");
+            }
         }
     }
 
